9c4c032f83c662c3bb275dfb6d8bb71d5288751d
katya <ekaterina.m.ostapenko@gmail.com>
2022-06-12 17:17:04 -0400

    First commit with utility packages

---
+++ src/test/java/com/cydeo/test/NextBaseCRM.java
@@ -0,0 +1,17 @@
+package com.cydeo.test;
+
+import com.cydeo.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class NextBaseCRM extends TestBase {
+    @Test
+    public void loginTest() {
+        //1.Go to home page
+        driver.get("https://login2.nextbasecrm.com/");
+
+    }
+}
